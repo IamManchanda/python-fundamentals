79d32412e7d01f332b66eeb2fb8ab8a8d17e3a68
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-07 17:23:25 +0530

    My First command line game in python completed!

--- rock-paper-scissors/game.py
+++ rock-paper-scissors/game.py
@@ -1,19 +1,36 @@
+from random import randint
+
+rand_num = randint(0, 2)
+if rand_num == 0:
+    computer = 'rock'
+elif rand_num == 1:
+    computer = 'paper'
+else:
+    computer = 'scissors'
+
+print("The available options are")
 print("...rock...")
 print("...paper...")
 print("...scissors...")
 
-player1 = input("Player 1, make your choice: ")
-print('*** No Cheating Please! ***\n\n' * 20)
-player2 = input("Player 2, make your choice: ")
+player = input("Hey Player, make your choice: ").lower()
+print(f"You played {player}")
+print(f"Computer plays {computer}")
 
-if player1 == player2: print("It's a tie")
-elif player1 == 'rock':
-    if player2 == 'scissors': print('Player 1, you won')
-    elif player2 == 'paper': print('Player 2, you won')
-elif player1 == 'paper':
-    if player2 == 'rock': print('Player 1, you won')
-    elif player2 == 'scissors': print('Player 2, you won')
-elif player1 == 'scissors':
-    if player2 == 'rock': print('Player 2, you won')
-    elif player2 == 'paper': print('Player 1, you won')
+def gameResult(winner):
+    if winner == 'player': print('Congratulations Player, you won this time')
+    elif winner == 'computer': print('Sorry, Computer got it right this time')
+    elif winner == 'tie': print("It's a tie")
+    else: print('Something went wrong')
+
+if player == computer: gameResult('tie')
+elif player == 'rock':
+    if computer == 'scissors': gameResult('player')
+    elif computer == 'paper': gameResult('computer')
+elif player == 'paper':
+    if computer == 'rock': gameResult('player')
+    elif computer == 'scissors': gameResult('computer')
+elif player == 'scissors':
+    if computer == 'rock': gameResult('computer')
+    elif computer == 'paper': gameResult('player')
 else: print('Something went wrong')
