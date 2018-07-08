8bb1d0a0d47d1828ed6a4e129c8c05ac01f526cf
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-09 01:36:10 +0530

    Lists!

---
+++ lists/for_loop2.py
@@ -0,0 +1,4 @@
+numbers = [4, 6, 2, 9, 7, 10]
+
+for number in numbers:
+    print(number * number)
---
+++ lists/while_loop.py
@@ -0,0 +1,6 @@
+colors = ['purple', 'teal', 'magenta', 'crimson', 'hotpink']
+
+index = 0
+while index < len(colors):
+    print(f"{index + 1}: {colors[index]}")
+    index += 1
